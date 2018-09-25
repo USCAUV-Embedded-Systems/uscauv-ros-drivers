51c818f88b68aace3ff48b671fd0c083a7f11819
Wenchao Wang <wenchao@usc.edu>
2018-09-24 22:15:00 -0700

    structure

--- utility_arduino/utility_arduino.ino
+++ utility_arduino/utility_arduino.ino
@@ -1,22 +1,30 @@
 #include <Wire.h>
 
 const int PIN_READ_PRESSURE = A0;
-const int SWITCH2 = 2;
-const int SWITCH3 = 3;
-const int SWITCH4 = 4;
-const int SWITCH5 = 5;
+const int PIN_READ_MOTORBAT = A1;
+const int PIN_READ_LOGICBAT = A2;
+const int SWITCH2 = D2;
+const int SWITCH3 = D3;
+const int SWITCH4 = D4;
+const int SWITCH5 = D5;
 //declare the pin
 
-int READ_SWITCH1;
 int READ_SWITCH2;
 int READ_SWITCH3;
 int READ_SWITCH4;
-int val = 0;
-int pressure = 0;
+int READ_SWITCH5;
+int val_pres = 0;
+int val_motor_batt = 0;
+int val_logic_batt = 0;
+double pressure = 0;
+double motor_battery = 0;
+double logic_battery = 0;
+
 //declare variables
 
-const int BUFFER_LEN = 4;
+const int BUFFER_LEN = 8;
 unsigned char i2c_buffer[BUFFER_LEN] = {'\0'};
+unsigned char button_state;
 //declare the buffer
 
 const int I2C_ADDRESS = 0x08; 
@@ -27,6 +35,8 @@ void setup()
   // put your setup code here, to run once:
   
   pinMode(PIN_READ_PRESSURE, INPUT);
+  pinMode(PIN_READ_MOTORBAT, INPUT);
+  pinMode(PIN_READ_LOGICBAT, INPUT);
   pinMode(SWITCH1, INPUT_PULLUP);
   pinMode(SWITCH3, INPUT_PULLUP);
   pinMode(SWITCH3, INPUT_PULLUP);
@@ -42,26 +52,34 @@ void setup()
 
 void loop() 
 {  
-    val = analogRead(PIN_READ_PRESSURE);
+    val_pres = analogRead(PIN_READ_PRESSURE);
     // read raw analogue value
-    //Serial.println(val);
+    //Serial.println(val_pres);
 
-    pressure = map(val, 95, 1023, 0, 103.42);
+    pressure = map(val_pres, 95, 1023, 0, 103.42);
     // project raw analogue value into kPa
     //Serial.print(pressure);
     //Serial.println(" kPa");
 
-    READ_SWITCH1 = digitalRead(SWITCH1);
+    val_motor_batt = analogRead(PIN_READ_MOTORBAT);
+
+    //motor_battery = map(val_motor_batt, );        //To be finished
+
+    val_logic_batt = analogRead(PIN_READ_LOGICBAT);
+
+    //logic_battery = map(val_logic_batt, );        //To be finished
+
     READ_SWITCH2 = digitalRead(SWITCH2);
     READ_SWITCH3 = digitalRead(SWITCH3);
     READ_SWITCH4 = digitalRead(SWITCH4);
+    READ_SWITCH5 = digitalRead(SWITCH5);
     // read switch switch status
-    //Serial.println(READ_SWITCH1);
     //Serial.println(READ_SWITCH2);
     //Serial.println(READ_SWITCH3);
     //Serial.println(READ_SWITCH4);
+    //Serial.println(READ_SWITCH5);
 
-    delay(100);  
+    delay(100);
 
 }
 
@@ -83,22 +101,41 @@ void receiveEvent(int numBytes)
   {
     for(int i = 0; i < BUFFER_LEN; i++)
     {
-      i2c_buffer[3 - i] = (pressure >> (i * 8));
+      i2c_buffer[BUFFER_LEN - 1 - i] = (pressure >> (i * 4));
     }
     // fill the buffer with pressure value
 
     Wire.write(i2c_buffer, BUFFER_LEN);
-    // respond with pressure of BUFFER_LEN bytes (int) and '\0' as sentinel
+    // respond with pressure of BUFFER_LEN bytes (double)
   } 
   else if (id == 'b')
   {
-    /*for(int i = 0; i < BUFFER_LEN; i++)
+    button_state = READ_SWITCH2 | (READ_SWITCH3 << 1) | (READ_SWITCH4 << 2) | (READ_SWITCH5 << 3);
+    // assemble the button state
+
+    Wire.write(button_state);
+    // respond with button state
+  }
+  else if (id == 'c')
+  {
+    for(int i = 0; i < BUFFER_LEN; i++)
     {
-      i2c_buffer[3 - i] = (temperature >> (i * 8));
+      i2c_buffer[BUFFER_LEN - 1 - i] = (motor_battery >> (i * 4));
     }
-    // fill the buffer with temperature value
+    // fill the buffer with motor battery value
 
     Wire.write(i2c_buffer, BUFFER_LEN);
-    // respond with temperature of BUFFER_LEN bytes (int) and '\0' as sentinel*/
+    // respond with motor battery voltage of BUFFER_LEN bytes (double)  
+  }
+  else if (id == 'd')
+  {
+    for(int i = 0; i < BUFFER_LEN; i++)
+    {
+      i2c_buffer[BUFFER_LEN - 1 - i] = (logic_battery >> (i * 4));
+    }
+    // fill the buffer with logic battery value
+
+    Wire.write(i2c_buffer, BUFFER_LEN);
+    // respond with logic battery voltage of BUFFER_LEN bytes (double) 
   }
 }
