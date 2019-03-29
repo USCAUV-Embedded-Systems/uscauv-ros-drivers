b7be61df6d64c4d6c3dd0cb630b310c95c31d639
Turtle <uscauv@gmail.com>
2019-03-28 22:12:42 -0700

    marker_dropper node is showing up but service is not, trying to start service with markerdropper.launch

--- ros_marker_dropper/src/marker_dropper.cpp
+++ ros_marker_dropper/src/marker_dropper.cpp
@@ -1,13 +1,18 @@
+#include "marker_dropper.h"
+#include <i2c_cpp/I2C.h>
 #include <ros/ros.h>
 #include "ros_marker_dropper/DropMarker.h"
+#include <std_srvs/Empty.h>
+#include <stdio.h>
 
-bool drop_marker(ros_marker_dropper::DropMarker::Request & request,
-                 ros_marker_dropper::DropMarker::Response & response){
-    I2C* i2c = new I2C(MODE1,  ARDUINO_ADDR);
+bool drop_marker(std_srvs::Empty::Request & request, std_srvs::Empty::Response & response){
+    //I2C* i2c = new I2C(MODE1,  ARDUINO_ADDR);
+    I2C i2c(1, 0x08);
     //should just write 0x42 to the arduino
-    i2c->write_byte(ARDUINO_ADDR, 0x42);
+    //i2c->write_byte(ARDUINO_ADDR, 0x42);
     //I don't know if this is enough, but we can dream 
-
+    
+    return true;
 }
 
 int main(int argc, char **argv){
@@ -16,7 +21,7 @@ int main(int argc, char **argv){
     
     ros::ServiceServer service = n.advertiseService("marker_dropper", drop_marker);
     ROS_INFO("Ready to drop marker");
+    std::cout << "Testing" << std::endl;
     ros::spin();
     return 0;
 }
-
--- ros_marker_dropper/src/marker_dropper.h
+++ ros_marker_dropper/src/marker_dropper.h
@@ -2,12 +2,11 @@
 #define MARKER_DROPPER_H
 
 #include <ros/ros.h>
-#include <i2c_cpp/I2C.h>
 
-#define MODE1 0x00
+
+#define MODE1 0x01
 #define ARDUINO_ADDR 0x08
 
-bool drop_marker(ros_marker_dropper::DropMarker::Request & request,
- ros_marker_dropper::DropMarker::Response & response);
+bool drop_marker();
  
 #endif
